f73576694b8e12b1d43336e6d2a866cf4384fc99
dzheng21 <19882922+dzheng21@users.noreply.github.com>
2023-01-27 16:40:36 -0500

    Add comments for CS178 review

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,32 @@
 <script>
+  // #StateVariable
   let todoItems = [];
+  // #StateVariable
   let newTodo = "";
 
   function addTodo() {
     newTodo = newTodo.trim();
     if (!newTodo) return;
 
+    // #StateVariable immutable
     const todo = {
       text: newTodo,
       checked: false,
       id: Date.now(),
     };
 
+    // #Reactive svelte will rerender each time todo items change (on assignment)
     todoItems = [...todoItems, todo];
+    // #Reactive empties DOM form input value field
     newTodo = "";
   }
   function toggleDone(id) {
+    // #ControlFlow related function — unpacks an array and applies a condition to elements
     const index = todoItems.findIndex((item) => item.id === Number(id));
     todoItems[index].checked = !todoItems[index].checked;
   }
   function deleteTodo(id) {
+    // #ControlFlow related function — unpacks an array and applies a condition to elements
     todoItems = todoItems.filter((item) => item.id !== Number(id));
   }
 </script>
@@ -35,6 +42,7 @@
         What do you want to get done today?
       </p>
     </div>
+    <!-- #EventHandler for form submission -->
     <form on:submit|preventDefault={addTodo}>
       <input
         class="js-todo-input"
@@ -46,15 +54,20 @@
     </form>
     <br />
     <ul style="columns:2" class="todo-list">
+      <!-- #ControlFlow "each" keywords allows todo items to render without an explicit loop -->
       {#each todoItems as todo (todo.id)}
         <li class="todo-item {todo.checked ? 'done' : ''}">
+          <!-- Example of #Properties -->
           <input id={todo.id} type="checkbox" />
+          <!-- #EventHandler for toggling done/todo -->
           <label
             for={todo.id}
             class="tick"
             on:click={() => toggleDone(todo.id)}
           />
+          <!-- #Reactive todo.text could change based on todo item, initial input -->
           <span>{todo.text}</span>
+          <!-- #EventHandler for deleting a todo list item -->
           <button class="delete-todo" on:click={() => deleteTodo(todo.id)}>
             <svg><use href="#delete-icon" /></svg>
           </button>
